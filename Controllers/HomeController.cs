2cae79ebe060f146f3be0d7f9685ffcca4e45a20
Dino <dinochang@taipay.com>
2022-02-15 16:48:33 +0800

    加入專案檔案。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using ExcelDataReader;
+using FileUplodDownLoadSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System.Diagnostics;
+
+namespace FileUplodDownLoadSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {         
+            return View();
+        }
+        [HttpPost]
+        public Tuple<string, int, int> ULFiles(List<IFormFile> files)
+        {
+            string errorMsg = "";
+            int  FailCount = 0, SuccessCount = 0,count=0;
+            foreach(var file in files)
+            {
+                count++;
+                string msg = file.FileName;
+                errorMsg += " <p class='mgB5'>";
+                errorMsg += "第" + count + "個檔案: " + msg + "。";
+                errorMsg += "</p>";
+            }
+            return Tuple.Create(errorMsg, SuccessCount, FailCount);
+        }
+
+        [HttpPost]
+        public Tuple<string, int, int> ULExcel(IFormFile excelFile)
+        {
+            int SuccessCount = 0, FailCount = 0, count = 0;
+            
+            string errorMsg = "";
+            if (excelFile == null)
+            {
+                errorMsg = "檔案不能是空的。";
+                return Tuple.Create(errorMsg, SuccessCount, FailCount);
+            }                
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = new MemoryStream())
+            {
+                excelFile.CopyTo(stream);
+                stream.Position = 0;
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read()) //Each row of the file
+                    {
+                        count++;
+                        if (count == 1)                        
+                            continue;   
+                        if (reader.GetValue(0) == null)
+                            break;
+                        var cell_1 = reader.GetValue(0);
+                        var cell_2 = reader.GetValue(1);
+                        var cell_3 = reader.GetValue(2);
+                        string msg = cell_1 + ";" + cell_2 + ";" + cell_3;
+                        errorMsg += " <p class='mgB5'>";
+                        errorMsg += "第" + (count - 1) + "筆資料: " + msg + "。";
+                        errorMsg += "</p>";                      
+                    }
+                }
+            }  
+            return Tuple.Create(errorMsg, SuccessCount, FailCount);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ wwwroot/js/upload.js
@@ -0,0 +1,128 @@
+﻿$("#importExlBTN").click(function () {
+    var input = document.getElementById("excelFile");
+    var files = input.files;
+    var formData = new FormData();
+    for (var i = 0; i < files.length; i++) {
+        formData.append("excelFile", files[i]);
+    }
+    var token = $("input[name='anticsrf']").val();
+    if (files.length > 0) {
+        $.ajax({
+            url: "Home/ULExcel",
+            type: "post",
+            contentType: false,
+            processData: false,
+            data: formData,
+            headers:
+            {
+                "RequestVerificationToken": token
+            },
+            success: function (result) {
+                if (result.item1 == "") {
+                    result.item1 = "全部資料成功上傳。";
+                }
+                //上傳資料結果
+                $("#resultMsg").html(result.item1);
+                $("#successCount").text(result.item2);
+                $("#failedCount").text(result.item3);
+                $("#importExlResultMessageModal").modal();
+            },
+            error: function () {
+                console.log("Error upload data ");
+            }
+        });
+    } else {
+        $("#resultMsg").html("檔案不能是空的。<br>");
+        $("#successCount").text("0");
+        $("#failedCount").text("0");
+        $("#importExlResultMessageModal").modal();
+    }
+});
+$("#importfileBTN").click(function () {
+    var input = document.getElementById("files");
+    var filerKit = $("#files").prop("jFiler");
+    var files = input.files;
+    var formData = new FormData();
+    for (var i = 0; i < files.length; i++) {
+        let flag = filerKit.files_list.some(function (item) {
+            if (item.file.name === files[i].name) {
+                return true;
+            }else{
+                return false;
+            }
+        });
+        if (flag) {
+            formData.append("files", files[i]);           
+        }        
+    }
+    var token = $("input[name='anticsrf']").val();
+    if (files.length > 0) {
+        $.ajax({
+            url: "Home/ULFiles",
+            type: "post",
+            contentType: false,
+            processData: false,
+            data: formData,
+            headers:
+            {
+                "RequestVerificationToken": token
+            },
+            success: function (result) {
+                if (result.item1 == "") {
+                    result.item1 = "全部資料成功上傳。";
+                }
+                $("#resultMsg").html(result.item1);
+                $("#successCount").text(result.item2);
+                $("#failedCount").text(result.item3);
+                $("#importExlResultMessageModal").modal();
+            },
+            error: function () {
+                console.log("Error upload data ");
+            }
+        });
+    } else {
+        $("#resultMsg").html("檔案不能是空的。<br>");
+        $("#successCount").text("0");
+        $("#failedCount").text("0");
+        $("#importExlResultMessageModal").modal();
+    }
+});
+
+$(function () {
+    $('#files').filer({
+        showThumbs: true,
+        addMore: true,
+        changeInput: true,
+        extensions: ["mp3", 'm4a'],
+        allowDuplicates: false,
+        templates: {
+            box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+            item: '<li class="jFiler-item">\
+                        <div class="jFiler-item-container">\
+                            <div class="jFiler-item-inner">\
+                                <div class="jFiler-item-thumb">\
+                                    <div class="jFiler-item-status"></div>\
+                                    <div class="jFiler-item-thumb-overlay">\
+                                        <div class="jFiler-item-info">\
+                                            <div style="display:table-cell;vertical-align: middle;">\
+                                                <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name}}</b></span>\
+                                                <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                            </div>\
+                                        </div>\
+                                    </div>\
+                                    {{fi-image}}\
+                                </div>\
+                                <div class="jFiler-item-assets jFiler-row">\
+                                    <ul class="list-inline pull-left">\
+                                        <li><input type="text" name="audioNotes" class="form-control form-control-sm" placeholder="備註欄填寫">{{fi-progressBar}}</li>\
+                                    </ul>\
+                                    <ul class="list-inline pull-right">\
+                                        <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </li>'
+        },
+    });
+});
